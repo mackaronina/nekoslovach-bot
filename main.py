d327d57d6dfc865d2c864ad7fbf8406e24a1b0dd
mishurov2021ks14 <mishurov2021ks14@student.karazin.ua>
2025-04-14 22:56:42 +0300

    some commit

--- main.py
+++ main.py
@@ -63,18 +63,62 @@ def news_text(new, new_tag):
     return f"⚡️<b>{new.title}</b>\n\n{new.text}\n\n#{new_tag.replace(' ', '_').replace('-', '_')}"
 
 
-class New(BaseModel):
+class NewModel(BaseModel):
     title: str
     text: str
 
 
-class Poll(BaseModel):
+class PollModel(BaseModel):
     question: str
     options: List[str]
 
 
 def post_image(file_id, channel=CHANNEL_CHATID):
     # пост с изображением в канал
+    new = generate_new_from_img(file_id)
+    bot.send_photo(channel, file_id, caption=news_text(new, 'предложка'))
+    poll = generate_poll(new.title)
+    bot.send_poll(
+        channel,
+        poll.question,
+        [InputPollOption(poll.options[0][:100]), InputPollOption(poll.options[1][:100])],
+        True
+    )
+
+
+def job_post_news(channel=CHANNEL_CHATID):
+    # пост в канал
+    new, new_tag = generate_new()
+    bot.send_message(channel, news_text(new, new_tag))
+    poll = generate_poll(new.title)
+    bot.send_poll(
+        channel,
+        poll.question,
+        [InputPollOption(poll.options[0][:100]), InputPollOption(poll.options[1][:100])],
+        True
+    )
+
+
+def generate_poll(title):
+    chat_completion = neuro.chat.completions.create(
+        messages=[
+            {"role": "system",
+             "content": LORE},
+            {"role": "user",
+             "content": f'Составь опрос к новости с таким заголовком: {title}. Опрос должен содержать ровно два \
+варианта ответа. Цель опроса это узнать мнение подписчиков канала касательно определённой темы. Опрос должен быть \
+провокационным чтобы в нём проголосовало как можно больше людей. Твой ответ должен быть в формате JSON с такими \
+полями: вопрос опроса, список из двух вариантов ответа. JSON должен соответствовать этой \
+схеме: {json.dumps(PollModel.model_json_schema(), indent=2)}'}
+        ],
+        response_format={"type": "json_object"},
+        model="meta-llama/llama-4-maverick-17b-128e-instruct",
+        temperature=TEMPERATURE
+    )
+    return PollModel.model_validate_json(chat_completion.choices[0].message.content)
+
+
+def generate_new_from_img(file_id):
     file_info = bot.get_file(file_id)
     downloaded_file = bot.download_file(file_info.file_path)
     base64_image = base64.b64encode(downloaded_file).decode('utf-8')
@@ -86,9 +130,10 @@ def post_image(file_id, channel=CHANNEL_CHATID):
              "content": [
                  {
                      "type": "text",
-                     "text": f'Напиши новость к которой идеально подойдёт это изображение. Учти что сегодня {cur_date().strftime("%d.%m.%Y")}, \
-но не добавляй эту дату в текст. Твой ответ должен быть в формате JSON с такими полями: заголовок новости, текст \
-новости. JSON должен соответствовать этой схеме: {json.dumps(New.model_json_schema(), indent=2)}'
+                     "text": f'Напиши новость к которой идеально подойдёт это изображение. Учти что сегодня \
+{cur_date().strftime("%d.%m.%Y")}, но не добавляй эту дату в текст. Твой ответ должен быть в формате JSON с такими \
+полями: заголовок новости, текст новости. JSON должен соответствовать этой схеме: \
+{json.dumps(NewModel.model_json_schema(), indent=2)}'
                  },
                  {
                      "type": "image_url",
@@ -102,24 +147,11 @@ def post_image(file_id, channel=CHANNEL_CHATID):
         model="meta-llama/llama-4-maverick-17b-128e-instruct",
         temperature=TEMPERATURE
     )
-    resp = New.model_validate_json(chat_completion.choices[0].message.content)
-    bot.send_photo(channel, file_id, caption=news_text(resp, 'предложка'))
+    return NewModel.model_validate_json(chat_completion.choices[0].message.content)
 
 
-def job_post_news(channel=CHANNEL_CHATID):
-    # пост в канал
-    new_tags = [
-        'спорт', 'финансы', 'технологии', 'международные отношения', 'политика', 'общество', 'наука',
-        'красота и здоровье', 'шоу-бизнес', 'компьютерные игры', 'погода', 'мнения', 'астрология', 'туризм',
-        'военные действия', 'культура', 'фильмы и сериалы', 'интервью', 'историческая правда', 'проишествия',
-        'власть', 'образование', 'криминал', 'разоблачение', 'благотворительность', 'мода', 'ИТ-технологии',
-        'кибербезопасность', 'изменение климата', 'медицина', 'выборы', 'коррупция', 'развлечения',
-        'социальные сети', 'мистика', 'кулинария', 'история успеха', 'литература и книги',
-        'развенчивание мифов', 'архитектура и урбанистика', 'беженцы и мигранты', 'аниме', 'терроризм',
-        'археология', 'страшная история', 'природа', 'забавная история', 'криптовалюта', 'лайфхаки',
-        'инсайдерская информация'
-    ]
-    new_tag = random.choice(new_tags)
+def generate_new():
+    new_tag = random.choice(NEW_TAGS)
     chat_completion = neuro.chat.completions.create(
         messages=[
             {"role": "system",
@@ -127,36 +159,13 @@ def job_post_news(channel=CHANNEL_CHATID):
             {"role": "user",
              "content": f'Напиши новость на тему "{new_tag}". Учти что сегодня {cur_date().strftime("%d.%m.%Y")}, \
 но не добавляй эту дату в текст. Твой ответ должен быть в формате JSON с такими полями: заголовок новости, текст \
-новости. JSON должен соответствовать этой схеме: {json.dumps(New.model_json_schema(), indent=2)}'}
+новости. JSON должен соответствовать этой схеме: {json.dumps(NewModel.model_json_schema(), indent=2)}'}
         ],
         response_format={"type": "json_object"},
         model="meta-llama/llama-4-maverick-17b-128e-instruct",
         temperature=TEMPERATURE
     )
-    resp = New.model_validate_json(chat_completion.choices[0].message.content)
-    bot.send_message(channel, news_text(resp, new_tag))
-    chat_completion = neuro.chat.completions.create(
-        messages=[
-            {"role": "system",
-             "content": LORE},
-            {"role": "user",
-             "content": f'Составь опрос к новости с таким заголовком: {resp.title}. Опрос должен содержать ровно два \
-варианта ответа. Цель опроса это узнать мнение подписчиков канала касательно определённой темы. Опрос должен быть \
-провокационным чтобы в нём проголосовало как можно больше людей. Твой ответ должен быть в формате JSON с такими \
-полями: вопрос опроса, список из двух вариантов ответа. JSON должен соответствовать этой \
-схеме: {json.dumps(Poll.model_json_schema(), indent=2)}'}
-        ],
-        response_format={"type": "json_object"},
-        model="meta-llama/llama-4-maverick-17b-128e-instruct",
-        temperature=TEMPERATURE
-    )
-    resp = Poll.model_validate_json(chat_completion.choices[0].message.content)
-    bot.send_poll(
-        channel,
-        resp.question,
-        [InputPollOption(resp.options[0][:100]), InputPollOption(resp.options[1][:100])],
-        True
-    )
+    return NewModel.model_validate_json(chat_completion.choices[0].message.content), new_tag
 
 
 @app.route(f'/{BOT_TOKEN}', methods=['POST'])
