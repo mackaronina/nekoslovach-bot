6983de430da9d9fe19bebe97c17f91592cf77146
mackaronina <mihamishu123@gmail.com>
2025-12-24 15:07:56 +0200

    Added logging for canceling post

--- app/handlers/callbacks.py
+++ app/handlers/callbacks.py
@@ -3,20 +3,21 @@ from aiogram.types import CallbackQuery
 
 from app.config import SETTINGS
 from app.keyboards.posting import keyboard_post_to_channel
-from app.utils.log import log_posting
+from app.utils.log import log_send_post, log_cancel_post
 
 router = Router()
 
 
 @router.callback_query(F.data == 'cancel')
 async def cancel_post(callback: CallbackQuery) -> None:
+    log_cancel_post(callback)
+    await callback.answer('Отменено')
     await callback.message.delete()
-    await callback.answer('Удалено')
 
 
 @router.callback_query(F.data == 'send')
 async def send_post(callback: CallbackQuery) -> None:
-    log_posting(callback)
+    log_send_post(callback)
     await callback.message.copy_to(SETTINGS.CHANNEL_CHAT_ID)
     await callback.answer('Отправлено')
     await callback.message.delete()
@@ -24,7 +25,7 @@ async def send_post(callback: CallbackQuery) -> None:
 
 @router.callback_query(F.data == 'send_confirm')
 async def send_to_confirm(callback: CallbackQuery) -> None:
-    log_posting(callback, True)
+    log_send_post(callback, True)
     await callback.message.copy_to(SETTINGS.ADMIN_CHAT_ID, reply_markup=keyboard_post_to_channel())
     await callback.answer('Отправлено на рассмотрение', show_alert=True)
     await callback.message.delete()
--- app/utils/log.py
+++ app/utils/log.py
@@ -3,7 +3,7 @@ import logging
 from aiogram.types import CallbackQuery
 
 
-def log_posting(callback: CallbackQuery, confirmation: bool = False) -> None:
+def log_send_post(callback: CallbackQuery, confirmation: bool = False) -> None:
     if callback.message.text is not None or callback.message.caption is not None:
         logging.info(
             f'User {callback.from_user.id} {"sending for confirmation" if confirmation else "posting"} new with text: {callback.message.text or callback.message.caption}'
@@ -11,4 +11,15 @@ def log_posting(callback: CallbackQuery, confirmation: bool = False) -> None:
     elif callback.message.poll is not None:
         logging.info(
             f'User {callback.from_user.id} {"sending for confirmation" if confirmation else "posting"} poll with question: {callback.message.poll.question}'
+        )
+
+
+def log_cancel_post(callback: CallbackQuery) -> None:
+    if callback.message.text is not None or callback.message.caption is not None:
+        logging.info(
+            f'User {callback.from_user.id} canceling new with text: {callback.message.text or callback.message.caption}'
+        )
+    elif callback.message.poll is not None:
+        logging.info(
+            f'User {callback.from_user.id} canceling poll with question: {callback.message.poll.question}'
         )
